6927be64d1d860d7f1c23ce201fbe8cc2750d149
ninjanye <ninjanye@gmail.com>
2020-03-08 21:32:35 +0000

    Add Vuetify and set up header

--- src/prophetsquad.core.web/src/App.vue
+++ src/prophetsquad.core.web/src/App.vue
@@ -1,32 +1,62 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app flat dense clipped-left>
+      <v-container class="px-xs-0" >
+        <v-row no-gutters>
+          <v-col>
+            <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer.show = !drawer.show"></v-app-bar-nav-icon>
+            <v-avatar tile class="hidden-sm-and-down">
+              <router-link to="/" tag="img" :src='require("./assets/logo.png")' :style="{ cursor: 'pointer'}" />
+            </v-avatar>
+            <v-btn text tile depressed height="100%" small to="fixtures" class="ml-3 hidden-sm-and-down">fixtures</v-btn>
+            <v-btn text tile depressed height="100%" small to="leagues" class="hidden-sm-and-down">leagues</v-btn>
+          </v-col>
+          <v-col cols="10" md="auto">
+            <SearchBar placeholder="Teams / Competitions..." />
+          </v-col>
+          <v-col align="right" class="hidden-sm-and-down">
+            <v-btn text tile depressed height="100%" small to="test">log in</v-btn>
+            <v-btn text tile depressed outlined color="primary" small to="register">sign up</v-btn>
+            <v-btn text tile depressed height="100%" small to="blog">blog</v-btn>
+            <v-btn text tile depressed height="100%" small to="about">about</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+
+    <v-navigation-drawer app clipped v-model="drawer.show" class="hidden-md-and-up">
+
+    </v-navigation-drawer>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+import SearchBar from "@/components/SearchBar.vue";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default {
+  name: "App",
+  components: {
+    SearchBar
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
+  data: () => ({
+    drawer: {
+      show: false
     }
-  }
-}
-</style>
+  })
+};
+</script>
---
+++ src/prophetsquad.core.web/src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-form v-on:submit="submit">
+    <v-text-field
+      full-width
+      single-line
+      dense
+      clearable
+      hide-details
+      prepend-inner-icon="mdi-magnify"
+      v-model="searchText"
+      :placeholder="placeholder"
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data: () => ({
+    searchText: null
+  }),
+  props: ["placeholder"],
+  methods: {
+    submit() {
+      this.$router.push("/search?" + this.searchText);
+    }
+  }
+};
+</script>
